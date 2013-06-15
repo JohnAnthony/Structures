4355085a9c96c66382ce6ae5d974a3402fe3e10f
John Anthony <johnanthony@lavabit.com>
2013-06-15 20:14:23 +0100

    Finished adding safe reverse

--- list.h
+++ list.h
@@ -246,10 +246,10 @@ list_splice_init(struct list_head *list, struct list_head *head) {
  * @member: the name of the list_struct within the struct.
  */
 #define list_for_each_entry_safe_reverse(pos, n, head, member) \
-    for (pos = list_entry((head)->next, typeof(*pos), member), \
-         n = list_entry(pos->member.next, typeof(*pos), member); \
+    for (pos = list_entry((head)->prev, typeof(*pos), member), \
+         n = list_entry(pos->member.prev, typeof(*pos), member); \
          &pos->member != (head); \
-         pos = n, n = list_entry(n->member.next, typeof(*n), member))
+         pos = n, n = list_entry(n->member.prev, typeof(*n), member))
 
 #endif
 #endif
