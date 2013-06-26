ce498c333522ade19bf6487d35d1161cf097101d
John Anthony <johnanthony@lavabit.com>
2013-06-27 00:53:24 +0100

    Silly little style change

--- list.h
+++ list.h
@@ -47,7 +47,7 @@ define LIST_HEAD(name) \
  */
 static inline void
 __list_add(struct list_head *new, struct list_head *prev,
-           struct list_head *next) {
+        struct list_head *next) {
     next->prev = new;
     new->next = next;
     new->prev = prev;
